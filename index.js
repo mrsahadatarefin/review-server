1f96897845397b8fe363f0b08b2d7defa8f73b76
mrsahadatarefin <sahadatarifin806@gmail.com>
2022-11-08 22:03:36 +0600

    service

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express =require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors =require('cors');
 const app = express();
 const port = process.env.PORT||5000;
@@ -27,6 +27,13 @@ app.get('/services',async(req,res)=>{
     const cursor= serviceCollection.find(query);
     const services =await cursor.toArray();
     res.send(services);
+
+})
+app.get('/service/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query ={_id:ObjectId(id)};
+    const service =await serviceCollection.findOne(query)
+    res.send(service);
 })
 }
 finally{
