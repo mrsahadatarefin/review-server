df05fc799e192467e0d46606298a773f70dec63d
mrsahadatarefin <sahadatarifin806@gmail.com>
2022-11-08 18:54:07 +0600

    port running

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express =require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors =require('cors');
 const app = express();
 const port = process.env.PORT||5000;
+require('dotenv').config();
 
 
 
 app.use(cors());
 app.use(express.json());
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w5yg5ut.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 app.get('/',(req,res)=>{
