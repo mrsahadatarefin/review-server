bf6260b466ce1edc35edea5f225fb9ceac7945d8
mrsahadatarefin <sahadatarifin806@gmail.com>
2022-11-09 00:20:20 +0600

    req

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
 try{
 const serviceCollection =client.db('review').collection('services');
- 
+ const orderCollection = client.db("review").collection('order');
 app.get('/services',async(req,res)=>{
 
     const query={}
@@ -34,7 +34,31 @@ app.get('/service/:id',async(req,res)=>{
     const query ={_id:ObjectId(id)};
     const service =await serviceCollection.findOne(query)
     res.send(service);
+});
+
+app.get('/orders', async(req,res)=>{
+
+
+    let query ={};
+
+    if(req.query.email){
+        query ={
+            email: req.query.email
+        }
+    }
+    const cursor = orderCollection.find(query);
+    const orders =await cursor.toArray();
+    res.send(orders);
+
 })
+
+app.post('/orders', async(req,res)=>{
+   const order= req.body;
+   const result = await orderCollection.insertOne(order);
+   res.send(result);
+
+})
+
 }
 finally{
 
